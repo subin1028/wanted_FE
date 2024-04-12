e3d90de7fb2a609402ae67f2da9072a3eb43601c
subin1028 <2lsbapple@gmail.com>
2024-04-12 20:33:46 +0900

    Feat: apply app

--- corp/T1/git-issue/src/App.js
+++ corp/T1/git-issue/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useContext} from 'react';
+import { DataContext } from './shared/context/context';
+import styled from "styled-components"
+import Headers from './shared/components/Headers';
+import Card from './shared/components/Card';
+import TCards from './shared/components/T_Card';
+
 
 function App() {
+  
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+        <DataContext>
+          <Headers/>
+          <TCards/>
+        </DataContext>
+      </div>
   );
 }
 
