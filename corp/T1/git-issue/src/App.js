9d54a38de3249e50ac36bc207ff145efe5e765e2
subin1028 <2lsbapple@gmail.com>
2024-04-13 16:41:26 +0900

    Test: delete unuse import codes

--- corp/T1/git-issue/src/App.js
+++ corp/T1/git-issue/src/App.js
@@ -1,9 +1,5 @@
 import React, {useContext} from 'react';
 import { DataContext } from './shared/context/context';
-import styled from "styled-components"
-import Headers from './shared/components/Headers';
-import Card from './shared/components/Card';
-import TCards from './shared/components/T_Card';
 import { RouterProvider } from 'react-router-dom';
 import router from "./shared/router/router";
 
