063f11b016f0e0ff1dddcf667ceee613674d8094
subin1028 <2lsbapple@gmail.com>
2024-04-13 14:05:20 +0900

    Feat: add Link style

--- corp/T1/git-issue/src/shared/components/T_Card.jsx
+++ corp/T1/git-issue/src/shared/components/T_Card.jsx
@@ -2,6 +2,7 @@ import React, {useContext} from "react";
 import { userContext } from "../context/context";
 import {RouterProvider, Link} from "react-router-dom";
 import Card from "./Card";
+import Headers from "./Headers";
 import styled from "styled-components";
 
 const TCards = (props) => {
@@ -9,19 +10,27 @@ const TCards = (props) => {
     console.log(detail[0]);
 
     return(
-        <Outerdetail>
-            {detail[0].map((item, id) => {
-                return <Card key={id} 
-                iss_num={item.number}
-                iss_title={item.title}
-                writer={item.user.login}
-                iss_date={DateFormatter(item.created_at)}
-                num_coment={item.comments}
-                ></Card>
-            })}
-        </Outerdetail>
+        <>
+            <Headers/>
+            <Outerdetail>
+                {detail[0].map((item, id) => {
+                    return (<Linkouter key={id} to={`/detail/:${id}`}><Card 
+                    iss_num={item.number}
+                    iss_title={item.title}
+                    writer={item.user.login}
+                    iss_date={item.created_at}
+                    num_coment={item.comments}
+                    ></Card></Linkouter>);
+                })}
+            </Outerdetail>    
+        </>
     )
 }
+
+const Linkouter = styled(Link)`
+    text-decoration: none;
+    color: black;
+`;
 
 const Outerdetail = styled.div`
     padding-top: 8vh;
@@ -30,17 +39,6 @@ const Outerdetail = styled.div`
     width: 90vw;
     margin: auto;
 `;
-
-function DateFormatter(origin_date){
-    const date = new Date(origin_date);
-
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-
-    const format_date = `${year}년 ${month}월 ${day}일`;
-    return format_date;
-};
 
   //이슈번호, 이슈제목, 작성자, 작성일, 코멘트수
   //0.number, 0.title, 0.user.login, 0.created_at, 0.comments
