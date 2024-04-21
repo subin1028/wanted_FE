0e79b2e6c8b728bfaedad1a7b2149b1d4f59b7ce
subin1028 <2lsbapple@gmail.com>
2024-04-21 17:29:41 +0900

    Design: add styled

--- corp/T2/exchange_corp/src/shared/components/Dropdown.jsx
+++ corp/T2/exchange_corp/src/shared/components/Dropdown.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCountry, addExchange } from "../store/redux_slice";
-import getFullDate from "./GetDate";
+import styled from "styled-components";
 
 const Dropdown = () => {
     const dispatch = useDispatch();
     const [value, setValue] = useState("USD");
-    const [date, setDate] = useState('');
-    const [change, setChange] = useState('');
-    const countryVal = useSelector((state) => state.redux_slice.countryVal);
     const inputVal = useSelector((state) => state.redux_slice.inputVal);
 
     const handleChange = (e) => {
@@ -23,16 +20,21 @@ const Dropdown = () => {
 
     return (
         <form>
-            <select value={value} onChange={handleChange}>
+            <Selectbox value={value} onChange={handleChange}>
                 <option value="USD">USD</option>
                 <option value="CAD">CAD</option>
                 <option value="KRW">KRW</option>
                 <option value="HKD">HKD</option>
                 <option value="JPY">JPY</option>
                 <option value="CNY">CNY</option>
-            </select>
+            </Selectbox>
         </form>
     );
 }
+
+const Selectbox = styled.select`
+    font-size: 2vw;
+    padding: 2px;
+`;
 
 export default Dropdown;
