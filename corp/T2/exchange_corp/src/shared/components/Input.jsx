bc73a826cfad0798938de85bff915a433af719f0
subin1028 <2lsbapple@gmail.com>
2024-04-21 17:30:51 +0900

    Feat: connect api from test code to api get

--- corp/T2/exchange_corp/src/shared/components/Input.jsx
+++ corp/T2/exchange_corp/src/shared/components/Input.jsx
@@ -9,37 +9,40 @@ import getFullDate from "./GetDate";
 const Input = (props) => {
     const dispatch = useDispatch();
     const [value, setValue] = useState('');
+    const [format_value, setFormat] = useState('');
     const [date, setDate] = useState('');
     const [change, setChange] = useState('');
     const country = useSelector((state) => state.redux_slice.countryVal);
-    const inputVal = useSelector((state) => state.redux_slice.inputVal);
 
     useEffect(() => {
         if (value.length >= 4 && country !== undefined){
-            // callApi([value, country]).then((data) => {
-            //     setChange(data.result);
-            //     setDate(getFullDate(data.date));                
-            // });
-            setChange(0.01 * value);
-            setDate(getFullDate("2024-04-21"));
+            callApi([value, country]).then((data) => {
+                setChange(data.result);
+                setDate(getFullDate(data.date));                
+            });
+    
             dispatch(setInput(value));
             dispatch(addExchange({ country, date, change, from:"input" }));
-            console.log("Input >>", country, date, change, "val>> ", value);
         }
     }, [value, date, country, change]);
     
     const onChange = (e) => {
         const newValue = e.target.value;
-        setValue(newValue);
+        setFormat(newValue);
+        setValue(newValue.replace(/,/g, ''));
     }
 
     return(
         <>
-            {/* <NumericFormat value={value} thousandSeparator={true} onChange={onChange} allowNegative={false}/> */}
-            <input type="number" value={value} onChange={onChange} onKeyDown={onChange}></input>
+            <Numeric value={format_value} thousandSeparator={true} onChange={onChange} allowNegative={false}/>
         </>
     )
 }
+
+const Numeric = styled(NumericFormat)`
+    margin-right: 20px;
+    font-size: 2vw;
+`
 
 
 export default Input;
