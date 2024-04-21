833631072870ae4d670d822563fbdf0ee8264cc2
subin1028 <2lsbapple@gmail.com>
2024-04-21 14:33:18 +0900

    Feat: Connect API

---
+++ corp/T2/exchange_corp/src/shared/network/callEC.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import exchangeApi from "../../apiKey";
+import { useDispatch, useSelector } from "react-redux";
+import fullDate from "../components/GetDate";
+
+const APIKey = exchangeApi.exchangeApi;
+// const dispatch = useDispatch();
+// const change = useSelector((state) => state.redux_slice.change);
+
+const instance = axios.create({
+    baseURL: "https://api.apilayer.com/exchangerates_data",
+    headers: {"apikey": APIKey},
+})
+
+export const callApi = async ([amount,country]) => {
+    try{
+        const response = await instance.get(`convert?to=${country}&from=KRW&amount=${amount}`);
+        console.log(response.data);
+        return await(response).data
+    }
+    catch(err){
+        console.log(err);
+    }
+}
