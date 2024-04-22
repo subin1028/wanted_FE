1f0b124c7725a56350c0998e5bf0bda138c55b46
subin1028 <2lsbapple@gmail.com>
2024-04-23 04:22:40 +0900

    Feat: add star svg and function

---
+++ corp/A1/my_condition/src/shared/components/Star.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components';
+import { ReactComponent as Estar } from '../../empty_star.svg';
+import { ReactComponent as FStar } from '../../full_star.svg';
+
+const Star = (props) => {
+  return (
+    <span role='button' onClick={props.onClick} onMouseEnter={props.onMouseEnter} onMouseLeave={props.onMouseLeave}>
+        {props.filled ? (
+            <FullStar/>
+        ) : (
+            <EmpStar/>
+        )}
+    </span>
+  )
+};
+
+const EmpStar = styled(Estar)`
+    fill: #fcc419;
+    width: 5vmax;
+    height: auto;
+`;
+
+const FullStar = styled(FStar)`
+    fill: #fcc419;
+    width: 5vmax;
+    height: auto;
+`;
+
+export default Star;
