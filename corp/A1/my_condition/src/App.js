e7c4be7243ec6e80e503184fd1e9a067b042e8c6
subin1028 <2lsbapple@gmail.com>
2024-04-23 04:23:33 +0900

    Feat: show star rate as table

--- corp/A1/my_condition/src/App.js
+++ corp/A1/my_condition/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+// import StarRate from "./shared/components/StarRate";
+import Table from "./shared/components/Table";
+import {StarMain, StarRate} from "./shared/components/StarRate";
 
 function App() {
   return (
     <div className="App">
-
+      <Table/>
     </div>
   );
 }
